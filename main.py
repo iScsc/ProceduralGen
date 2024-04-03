cfe31fb74e1bc272f271aeb6aa2fc7441157a4ca
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-04-03 19:38:29 +0200

    Single map case

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ print("SEED =", SEED)
 
 mapGenerator.setSeed(SEED)
 
-MAX_LENGTH = 4
+MAX_LENGTH = 1 #4
 
 #20 first prime numbers
 prime_numbers = np.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 43, 47, 53, 59, 61, 67, 71, 73])
 
-GRID_SIZE = np.array([prime_numbers[2*i] for i in range(0, min(len(prime_numbers)//2, MAX_LENGTH) ) ] )  #np.array([2, 15, 35, 80]) #np.array([5,8,10,15,20,36])
-VALUE_FACTOR = np.array([1/i**2 for i in range(1, len(GRID_SIZE) + 1)])  #np.array([100, 10, 1, 1]) #np.array([.9, .7, .6])
+GRID_SIZE = 20
+VALUE_FACTOR = 1
+#GRID_SIZE = np.array([prime_numbers[2*i] for i in range(0, min(len(prime_numbers)//2, MAX_LENGTH) ) ] )  #np.array([2, 15, 35, 80]) #np.array([5,8,10,15,20,36])
+#VALUE_FACTOR = np.array([1/i**2 for i in range(1, len(GRID_SIZE) + 1)])  #np.array([100, 10, 1, 1]) #np.array([.9, .7, .6])
 ppcm = ppcm_of_list(GRID_SIZE)
-print(ppcm)
 
 
 SEA_LEVEL = -0.15
