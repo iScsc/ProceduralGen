ace5beaad7a5b6895cde98d0390e49ab398aa709
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-04-20 11:24:05 +0200

    Add more colors

--- C/loadingBar.h
+++ C/loadingBar.h
@@ -4,8 +4,21 @@
 // ##### Definitions ####################
 
 // ----- Colors
-#define GREEN_COLOR "\033[92m" //"\e[0;32m" //'\033[92m'
 #define DEFAULT_COLOR "\033[0m"
+
+#define GREEN_COLOR "\033[92m"
+#define RED_COLOR "\033[91m"
+#define YELLOW_COLOR "\033[93m"
+#define BLUE_COLOR "\033[94m"
+#define PURPLE_COLOR "\033[95m"
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 // ----- Characters
 #define HORIZONTAL_RECTANGLE_CHAR '-'   //"\u25ac"
