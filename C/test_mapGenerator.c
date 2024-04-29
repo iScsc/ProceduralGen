52a778d73f68625de3603a6380cdccf48d8a7673
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-04-29 14:38:11 +0200

    A bit of time tracking

--- C/test_mapGenerator.c
+++ C/test_mapGenerator.c
@@ -69,16 +69,19 @@ int main()
     // fullGen testing
     if (complete_map_generation_testing == 1)
     {
+        clock_t start_time = clock();
+
         setRandomSeed(time(NULL)); //? Comment this to make it not random, or give a constant rather than time(NULL)
 
-        int nb_layers = 2;
+        int nb_layers = 3;
 
-        int dimensions[] = {3, 7};
+        int dimensions[] = {2, 5, 7}; //, 11};
+        int final_size = lcmOfArray(nb_layers, dimensions);
 
-        double weights[] = {1, .1};
+        double weights[] = {1, .1, .01}; //, 0.001};
 
-        int width = 3;
-        int height = 2;
+        int width = 7;
+        int height = 5;
 
         double sea_level = -.1;
 
@@ -90,6 +93,9 @@ int main()
 
         printf("Deallocating now...\n");
         freeCompleteMap(new_complete_map);
+
+        double total_time = (double) (clock() - start_time) / CLOCKS_PER_SEC;
+        printf("The whole map generation of lcm %d took a total of %lf s\n", final_size, total_time);
     }
 
     return 0;
