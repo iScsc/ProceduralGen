719f4f538bd2b40e9fe4c3d055791a6c652a3b63
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-05-01 15:34:52 +0200

    Testing loading bars

--- C/test_loadingBar.c
+++ C/test_loadingBar.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-#include <wchar.h> // Used for unicode characters (but does not work yet)
-#include <string.h> // For str concatenation
+#include <time.h>
 
 #include "loadingBar.h"
 
 int main()
 {
-    // printf("Testing unicode characters:\n");
-
-    // wchar_t recantgle = 0x25ad;
-    // printf("- This should be a rectangle : ");
-    // wprintf(L"%lc\n", recantgle);
-
-    // printf("\nTest : %c", 2588);
-
     char begin_char[1 + 7 + 1] = "\r";
     
     // get end of the begin_char
@@ -33,20 +24,30 @@ int main()
     }
     begin_char[k + j] = '\0';
 
-    // strncat(begin_char, ANSI_COLOR_MAGENTA, 7);
-
     printf("Voici %s quelque chose qui est un %stest de couleur.%s\n", begin_char, RED_COLOR, DEFAULT_COLOR);
 
     printf("\nTesting loading bar:\n");
 
-    for (int i = 0; i <= 1000; i++)
+    for (int i = 0; i <= 500; i++)
     {
-        print_loading_bar(i, 1000, 100, begin_char, DEFAULT_COLOR);
-        fflush(stdout); // Allows printing without '\n'
+        print_loading_bar(i, 500, 100, begin_char, DEFAULT_COLOR);
         delay(.01);
     }
 
     printf("\nFinished!\n");
+
+    int indent_level = 2;
+
+    indent_print(indent_level - 1, "Testing the predefined loading bar and the indent print:\n");
+
+    char base[100] = "This is a testing loading bar ";
+    double start_time = (double) clock();
+
+    for (int i = 0; i <= 500; i++)
+    {
+        predefined_loading_bar(i, 500, 50, base, indent_level, start_time);
+        delay(.01);
+    }
 
     return 0;
 }
