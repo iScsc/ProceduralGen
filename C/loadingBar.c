d305d1d6567726e2262f7de8c0ce08b6a213bbac
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-05-21 14:32:12 +0200

    Add loadingBar documentation

--- C/loadingBar.c
+++ C/loadingBar.c
@@ -1,8 +1,24 @@
+/**
+ * @file loadingBar.c
+ * @author Zyno
+ * @brief loading bar functions implementation
+ * @version 0.1
+ * @date 2024-05-21
+ * 
+ */
+
 #include <stdio.h>
 #include <time.h>
 
 #include "loadingBar.h"
 
+/**
+ * @brief A simple function to raise a double to an integer power.
+ * 
+ * @param value (double) : the input value
+ * @param power (int) : the integer power
+ * @return double : value ** power
+ */
 double pow_int(double value, int power)
 {
     if (power == 0)
@@ -34,7 +50,7 @@ void delay(float waiting_seconds)
 
     while( (double)(clock() - start_time) / CLOCKS_PER_SEC < waiting_seconds)
     {
-        ;
+        ; // Just waiting...
     }
 }
 
@@ -43,6 +59,8 @@ void delay(float waiting_seconds)
 void indent_print(int number_of_indents, char str[])
 {
     int indent_length = number_of_indents * CUSTOM_INDENT_WIDTH;
+
+    // Generates a custom indent to be printed
     char begin_indent[indent_length + 1];
     
     for (int k = 0; k < number_of_indents; k++)
@@ -68,20 +86,22 @@ void indent_print(int number_of_indents, char str[])
 
 
 
-void print_loading_bar(int value, int max_value, int number_of_segments, char* pre_text, char* post_text)
+void print_loading_bar(int value, int max_value, int number_of_segments, char pre_text[], char post_text[])
 {
     char begin_char = '[';
     char end_char = ']';
     char loading_char = HORIZONTAL_RECTANGLE_CHAR;
 
+    // Creates the correct format with the constant PERCENT_DECIMAL precision
     char format[20] = "";
     snprintf(format, sizeof(format), "%s%d%s", "%s%s%c%3.", PERCENT_DECIMAL, "f%%%s");
 
 
-
+    // Loading bar actual parameters
     float value_per_segment = (float) max_value / number_of_segments;
     int number_filled = value / value_per_segment;
 
+    // The loading bar is number_of_segments long + 3 for '[', ']' and '\0'
     char loading_bar[number_of_segments + 3];
     loading_bar[0] = begin_char;
 
@@ -109,18 +129,20 @@ void print_loading_bar(int value, int max_value, int number_of_segments, char* p
 
     float percent = (value * 100.) / max_value;
 
+    // Printing the result
     printf(format, pre_text, loading_bar, INDENT_CHAR, percent, post_text);
 }
 
 
 
-void predefined_loading_bar(int value, int max_value, int number_of_segments, char* base_str, int number_of_indents, double start_time)
+void predefined_loading_bar(int value, int max_value, int number_of_segments, char base_str[], int number_of_indents, double start_time)
 {
     float previous_percent = ((value - 1) * 100.) / max_value;
     float current_percent = (value * 100.) / max_value;
 
     // Verifying first if it useful to print it.
     //! Won't work properly if it used on very long loops -> the CPU elapsed time will 'jump' from values to others when a percent changes.
+    //! But a very long loop in C means the map is gigantic and, in our case, would not even be able to generate because it would take way too much memory.
 
     double precision = pow_int(10., PERCENT_DECIMAL);
     double truncated_prev = ((int) previous_percent * precision)/precision;
@@ -134,8 +156,10 @@ void predefined_loading_bar(int value, int max_value, int number_of_segments, ch
 
 
 
+    // Stylish loading bar preparation
     double current_time = (double) (clock() - start_time)/CLOCKS_PER_SEC;
 
+    // Preparing the post_text
     char end_str[1000] = "";
 
     char time_str[100] = "";
@@ -150,9 +174,11 @@ void predefined_loading_bar(int value, int max_value, int number_of_segments, ch
         snprintf(end_str, sizeof(end_str), "%s", time_str);
     }
 
+    // Preparing the pre_text
     char begin_str[1000] = "";
     snprintf(begin_str, sizeof(begin_str), "\r%s", base_str);
 
+    // Manually generating the indents to be able to use the `print_loading_bar` function
     int indent_length = number_of_indents * CUSTOM_INDENT_WIDTH;
     char begin_indent[indent_length + 1];
     
@@ -174,5 +200,6 @@ void predefined_loading_bar(int value, int max_value, int number_of_segments, ch
 
     snprintf(begin_str, sizeof(begin_str), "\r%s%s", begin_indent, base_str);
 
+    // Printing the final result
     print_loading_bar(value, max_value, NUMBER_OF_SEGMENTS, begin_str, end_str);
 }
