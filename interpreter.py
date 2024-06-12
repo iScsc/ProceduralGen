e3d561c74705ab108e2ddcc3e2a5dfdf2c1b4e96
BlueNZ <zadigballesio@gmail.com>
2024-06-12 20:25:28 +0200

    types primitifs

--- interpreter.py
+++ interpreter.py
@@ -1,22 +1,27 @@
 
-def encode(object : object) -> str:
+def encode(object : object, indent : int=0) -> str:
     
     obj_class = object.__class__
-    
-    #? Match base types
-    
-    
-    #? If object :
-    
-    obj_dict = object.__dict__
-    
     
     obj_str = "{"
     obj_str += "\n\t__class__: " + str(obj_class).split("'")[1].split(".")[-1]
     
     
-    for arg in obj_dict:
-        obj_str += ",\n\t" + arg + ": " + str(object.__getattribute__(arg))
+    #? Match base types
+    
+    obj_type = type(object)
+    
+    if (obj_type in [int, float, complex, list, tuple, str, bytearray, bytes]):
+        obj_str += "\n\tvalue: " + str(object)
+    
+    
+    #? If object :
+    
+    else :
+        obj_dict = object.__dict__ 
+           
+        for arg in obj_dict:
+            obj_str += ",\n\t" + arg + ": " + str(object.__getattribute__(arg))
     
     
     obj_str += "\n}"
