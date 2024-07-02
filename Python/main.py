4a8a01a9f7ddfe484dfc0b0b5cde1fbc3f5c0334
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-07-02 23:58:44 +0200

    Make main use new MapGenerator in GENERATE part

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,10 @@
 import random as rd
-
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from old_mapGenerator import *
+from old_mapGenerator import mapGenerator as old_MapGenerator
+from mapGenerator import MapGenerator
 
 LOAD = False
 GENERATE = True
@@ -11,19 +12,19 @@ GENERATE = True
 if LOAD:
     path_to_load = "../saves/completeMap_test/"
     
-    sea_map, color_map, map_width_in_points, map_height_in_points, sea_level, map_width_in_chunks, map_height_in_chunks = mapGenerator.loadCompleteMap(path_to_load)
+    sea_map, color_map, map_width_in_points, map_height_in_points, sea_level, map_width_in_chunks, map_height_in_chunks = old_MapGenerator.loadCompleteMap(path_to_load)
     
     # print(sea_map)
     # print(color_map)
     
-    mapGenerator.printMap((map_width_in_chunks, map_height_in_chunks), (map_width_in_points, map_height_in_points), sea_level, sea_map, color_map)
+    old_MapGenerator.printMap((map_width_in_chunks, map_height_in_chunks), (map_width_in_points, map_height_in_points), sea_level, sea_map, color_map)
 
 
 
 if GENERATE:
     start = time.time()
-    #SEED = 909729265464 #rd.randint(0, 1000000000000)
-    SEED = 1714995019 #mapGenerator.getRandSeed() #1714995019 #436517554376 # mapGenerator.getRandSeed()
+    
+    SEED = MapGenerator.getRandomSeed() #1714995019 #436517554376
     # Interesting seeds :
     # 909729265464
     # 436517554376
@@ -33,25 +34,30 @@ if GENERATE:
     SEED = "seed"
     print("SEED =", SEED)
 
-    mapGenerator.setSeed(SEED)
+    MapGenerator.setRandomSeed(SEED)
 
     MAX_LENGTH = 3
 
     #20 first prime numbers
     prime_numbers = np.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 43, 47, 53, 59, 61, 67, 71, 73])
 
-    GRID_SIZE = [3, 5] #[2, 5, 11] #np.array([prime_numbers[i] for i in range(0, min(len(prime_numbers), MAX_LENGTH) ) ] ) #np.array([prime_numbers[2*i] for i in range(0, min(len(prime_numbers)//2, MAX_LENGTH) ) ] )  #np.array([2, 15, 35, 80]) #np.array([5,8,10,15,20,36])
-    VALUE_FACTOR = [1, 0.1] #[1, 0.1, 0.01] #np.array([1/i**2 for i in range(1, len(GRID_SIZE) + 1)])  #np.array([100, 10, 1, 1]) #np.array([.9, .7, .6])
+    GRID_SIZES = [3, 5, 9]                  #[2, 5, 11] #np.array([prime_numbers[i] for i in range(0, min(len(prime_numbers), MAX_LENGTH) ) ] ) #np.array([prime_numbers[2*i] for i in range(0, min(len(prime_numbers)//2, MAX_LENGTH) ) ] )  #np.array([2, 15, 35, 80]) #np.array([5,8,10,15,20,36])
+    LAYERS_FACTORS = [1, 0.1, 0.01]         #[1, 0.1, 0.01] #np.array([1/i**2 for i in range(1, len(GRID_SIZE) + 1)])  #np.array([100, 10, 1, 1]) #np.array([.9, .7, .6])
 
 
-    MAP_SIZE = (3, 2) #(1,1) #(5, 7)
+    MAP_SIZE = (3, 2)                       #(1,1) #(5, 7)
 
 
-    ppcm = ppcm_of_list(GRID_SIZE)
+    lcm = MapGenerator.lcmOfArray(GRID_SIZES)
 
 
-    SEA_LEVEL = 0
-
-
-    full_map, sea_map, cmap_float = mapGenerator.fullGen(GRID_SIZE, VALUE_FACTOR, SEA_LEVEL, map_size=MAP_SIZE, display_loading = True, display_map = True)
+    SEA_LEVEL = -0.3
+    
+    
+    new_map = MapGenerator.fullGen(GRID_SIZES, LAYERS_FACTORS, MAP_SIZE[0], MAP_SIZE[1], SEA_LEVEL)
+    
+    
     print("The whole generation took {} seconds.".format(time.time() - start))
+    print("Plotting now...")
+    
+    MapGenerator.plotCompleteMap(new_map)
