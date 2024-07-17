fbc25d928de1b53c111732dcb9fe6c22c40c9765
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-07-18 00:10:06 +0200

    Add a moving camera object

---
+++ Godot/scripts/moving_camera.gd
@@ -0,0 +1,41 @@
+class_name MovingCamera extends Camera3D
+
+# CONSTANTS
+
+const SPEED := 5.0
+const ROTATION_SPEED := 5e-2
+
+const CAMERA_LIMITS := Vector2(-PI/2, PI/2)
+
+func _init():
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+
+
+
+func _physics_process(delta):
+	if DisplayServer.window_is_focused():
+		var input_rot: Vector2 = get_viewport().get_mouse_position() - Vector2(get_viewport().size/2)
+		var rot := Vector3(input_rot.y, input_rot.x, 0)
+		if rot:
+			rotation -= rot * ROTATION_SPEED * delta
+			rotation.x = clamp(rotation.x, CAMERA_LIMITS.x, CAMERA_LIMITS.y)
+	
+	
+	## Handle rising
+	if Input.is_action_pressed("jump"):
+		position.y += SPEED * delta
+	## Handle lowering
+	if Input.is_action_pressed("crouch"):
+		position.y -= SPEED * delta
+	
+	
+	## Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	position += direction * SPEED * delta
+	
+	
+	## Keep cursor in the middle of the screen
+	if DisplayServer.window_is_focused():
+		get_viewport().warp_mouse(get_viewport().size/2)
