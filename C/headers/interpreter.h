102d7e6b6ea795f5198cb88c913f5aa716f0ea08
BlueNZ <zadigballesio@gmail.com>
2024-07-27 22:14:03 +0200

    C basic file structure

---
+++ C/headers/interpreter.h
@@ -0,0 +1,19 @@
+/**
+ * @file interpreter.h
+ * @author BlueNZ
+ * @brief Header to interpreter functions
+ * @version 0.1
+ * @date 2024-07-27
+ * 
+ */
+
+#ifndef CHUNK
+#define CHUNK
+
+// ------- Structure definition ------- //
+
+typedef char byte;
+
+
+
+#endif
---
+++ C/src/interpreter.c
@@ -0,0 +1,16 @@
+/**
+ * @file interpreter.c
+ * @author BlueNZ
+ * @brief interpreter functions implementation
+ * @version 0.1
+ * @date 2024-07-27
+ * 
+ */
+
+
+#include "interpreter.h"
+
+
+
+// ------- Binary functions ------- //
+
